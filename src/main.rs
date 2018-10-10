c91f68b329974b265d4d061fa649fca99ce88a01
yoisyosi <yosiyosi3632@gmail.com>
2018-10-10 15:09:48 +0900

    modularization

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,38 +16,9 @@ fn main() {
     println!("searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application Error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            //ユーザーが値を入力するような提供のためのコードはnewの条件をpanicにするのではなくErrにするのが望ましい。開発者相手のlib開発にはpanicが望ましい。
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
