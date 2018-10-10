01bb503b7ead6da5f9ea6cbbab9c43191260a7a0
yoisyosi <yosiyosi3632@gmail.com>
2018-10-10 17:31:46 +0900

    Implement search

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application Error: {}", e);
 
