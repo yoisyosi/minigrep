152afa647da24583a8d892cdb3fe9dcdb56f0696
yoisyosi <yosiyosi3632@gmail.com>
2018-10-10 13:03:01 +0900

    Implement Config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {}", err);
+        //panicと違いRUST_BAKTRACEなど余計な物を出さずにプログラミングを終了できる
+        process::exit(1);
+    });
+
     println!("searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -24,10 +30,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            //ユーザーが値を入力するような提供のためのコードはnewの条件をpanicにするのではなくErrにするのが望ましい。開発者相手のlib開発にはpanicが望ましい。
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
